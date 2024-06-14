4df6cdd0dab989a42a7c181767fe3c4de322fa44
Chaitralikore <127185062+Chaitralikore@users.noreply.github.com>
2024-06-14 20:43:25 +0530

    Time: 11 ms (90.68%), Space: 21.6 MB (38.59%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+ 
+    void moveZeroes(vector<int>& nums) {
+        
+        int lastNonZeroFoundAt = -1;
+       
+        int n = nums.size();
+      
+       
+        for (int currentIndex = 0; currentIndex < n; ++currentIndex) {
+            
+            if (nums[currentIndex] != 0) {
+                
+                swap(nums[++lastNonZeroFoundAt], nums[currentIndex]);
+            }
+            
+        }
+        
+    }
+};
